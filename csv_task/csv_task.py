f24fcbeb20d914cd18dbb493319d625644c2962b
siyam04 <galib.abdullah04@gmail.com>
2020-02-10 11:43:16 +0600

    csv file done

--- csv_task/csv_task.py
+++ csv_task/csv_task.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 
 with open('FL_insurance_sample.csv', 'r') as csv_file:
-    # reader = csv.reader(csv_file, skipinitialspace=True)
+    # reader = csv.reader(csv_file, delimiter='|')
     reader = csv.reader(csv_file, skipinitialspace=True)
 
     for row in reader:
