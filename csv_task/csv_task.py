477c4c5a7d18f8eafca3ee0872af8aecca8fe06b
siyam04 <galib.abdullah04@gmail.com>
2020-02-10 11:47:53 +0600

    csv file reading in dictionary format

--- csv_task/csv_task.py
+++ csv_task/csv_task.py
@@ -4,11 +4,15 @@ from pprint import pprint
 
 with open('FL_insurance_sample.csv', 'r') as csv_file:
     # reader = csv.reader(csv_file, delimiter='|')
-    reader = csv.reader(csv_file, skipinitialspace=True)
+    # reader = csv.reader(csv_file, skipinitialspace=True)
+    dict_reader = csv.DictReader(csv_file)
 
-    for row in reader:
+    for row in dict_reader:
+        print(dict(row))
+
+    # for row in reader:
         # pprint(row)
-        print(row)
+        # print(row)
 
 
 
